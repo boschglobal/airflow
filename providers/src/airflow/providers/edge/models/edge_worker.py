7d532ea4ad0601cd6962f5b88a9afb82925ad3d6
Majoros Donat (XC-DX/EET2-Bp) <donat.majoros2@hu.bosch.com>
2024-10-17 14:01:48 +0200

    Send other necessary metrics

--- providers/src/airflow/providers/edge/models/edge_worker.py
+++ providers/src/airflow/providers/edge/models/edge_worker.py
@@ -196,7 +196,10 @@ class EdgeWorker(BaseModel, LoggingMixin):
         worker.jobs_active = jobs_active
         worker.sysinfo = json.dumps(sysinfo)
         worker.last_update = timezone.utcnow()
-        Stats.incr(f'edge_worker.{worker_name}.heartbeat_count', 1, 1)
+        Stats.incr(f'edge_worker.heartbeat_count.{worker_name}', 1, 1)
+        Stats.gauge('edge_worker.state', state , tags={"worker_name": worker_name})
+        Stats.gauge('edge_worker.active_jobs', jobs_active , tags={"worker_name": worker_name})
+        Stats.gauge('edge_worker.queues', worker.queue.strip() , tags={"worker_name": worker_name})
         session.commit()
 
 
